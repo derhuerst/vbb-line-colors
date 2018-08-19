d30e099ea594ae628b41dfcf8f0e44dd3e5bf155
Jannis R <mail@jannisr.de>
2018-08-19 04:18:20 +0200

    basic test :white_check_mark:

--- test.js
+++ test.js
@@ -1,5 +1,25 @@
 'use strict'
 
+const hexColorRegex = require('hex-color-regex')
 const a = require('assert')
 
-// todo
+const lineColors = require('.')
+
+const isObj = o => o !== null && 'object' === typeof o && !Array.isArray(o)
+
+a.ok(isObj(lineColors))
+a.ok(Object.keys(lineColors).length > 0)
+
+for (const product of Object.keys(lineColors)) {
+	const byProduct = lineColors[product]
+	a.ok(isObj(byProduct), product)
+	a.ok(Object.keys(byProduct).length > 0, product)
+
+	for (const line of Object.keys(byProduct)) {
+		const colors = byProduct[line]
+		a.ok(isObj(colors), product + ' ' + line)
+
+		a.ok(hexColorRegex().test(colors.fg), product + ' ' + line)
+		a.ok(hexColorRegex().test(colors.bg), product + ' ' + line)
+	}
+}
